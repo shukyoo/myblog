3f8ee4744fb183a1b75c11a245310da582388c19
Fredyang <shuky2000@163.com>
2015-01-19 22:27:00 +0800

    lost in devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout :layout
+
+  private
+
+  def layout
+    # only turn it off for login pages:
+    is_a?(Devise::SessionsController) ? false : "application"
+    # or turn layout off for every devise controller:
+    # devise_controller? && "application"
+  end
 end
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,4 +2,6 @@ class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :rememberable, :trackable, :timeoutable, :lockable
+  validates_format_of :email,:with => Devise.email_regexp
+  # validates :email, :presence => true, :email => true
 end
