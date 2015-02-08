412f07963040ee409b6ca5e28e74da9a29136c2d
Fredyang <shuky2000@163.com>
2015-02-08 15:56:37 +0800

    增加修改密码

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -60,6 +61,9 @@ Rails.application.routes.draw do
     root "index#index"
     get '/login', to: 'auth#login'
     get '/test', to: 'test#index'
+
+    get '/user/edit_password' => 'users#edit_password', as: 'user_edit_password'
+    post '/user/update_password' => 'users#update_password', as: 'user_update_password'
 
     resources :articles
     resources :categories
