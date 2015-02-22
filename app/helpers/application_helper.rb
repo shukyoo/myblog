66bdf32e92fb366456d462aae2335283eb2302ed
Fredyang <shuky2000@163.com>
2015-02-22 22:20:41 +0800

    fix page views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def plaintext_summary(text, length: 280)
+    doc = Nokogiri::HTML::DocumentFragment.parse(text)
+    truncate(doc.css('p').map(&:text).join('').to_s, :length => length)
+  end
 end
