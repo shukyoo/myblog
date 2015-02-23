6a4ec6b57a06c841f43458d1dde90ea57901813f
Fredyang <shuky2000@163.com>
2015-02-23 15:19:59 +0800

    fix timezone

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Myblog
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.encoding = "utf-8"
+    config.time_zone = 'Beijing'
   end
 end
