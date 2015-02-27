5bd8830b728fb2eb241341f35bc1c981082d437f
Fredyang <shuky2000@163.com>
2015-02-27 21:11:14 +0800

    up devise timeout to 8hours

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,7 +1,7 @@
 class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :rememberable, :trackable, :timeoutable, :lockable
+  devise :database_authenticatable, :rememberable, :trackable, :timeoutable, :lockable, :timeout_in => 8.hours
   validates_format_of :email,:with => Devise.email_regexp
   # validates :email, :presence => true, :email => true
 end
